e703897e775c3b041dbfa615e3e5cd290b27121d
Erik Zachrisson <erik@zachrisson.info>
2016-05-27 19:01:23 +0200

    Declare correct ports

--- clk_and_reset.vhd
+++ clk_and_reset.vhd
@@ -5,15 +5,30 @@ use ieee.std_logic_unsigned.all;
 
 use work.Types.all;
 
-entity ClkAndReset is 
-	port (
-	AsyncRst : in bit1;
-	Clk      : in bit1;
-	--
-	Rst_N    : out bit1
-	);
+entity clk_and_reset is
+        port (
+        sys_clk_pad_i  : in  bit1;
+        tck_pad_i      : in  bit1;
+        eth_clk_pad_i  : in  bit1;
+        rst_pad_i      : in  bit1;
+        soft_reset_i   : in  bit1;
+        --
+        sdram_clk_o    : out bit1;
+        dbg_tck_o      : out bit1;
+        --
+        wb_clk_o       : out bit1;
+        wb_rst_o       : out bit1;
+        --
+        eth_clk_o      : out bit1;
+        eth_rst_o      : out bit1;
+        eth0_phy_rst_n : out bit1;
+        --
+        tc_clk_o       : out bit1;
+        --
+        sys_rst_o      : out bit1
+        );
 end entity;
 
-architecture rtl of ClkAndReset is
+architecture rtl of clk_and_reset is
 begin
 end architecture rtl;
