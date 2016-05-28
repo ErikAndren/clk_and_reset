33641c736279e4152f27ec77d3e0750110c22f6e
Erik Zachrisson <erik@zachrisson.info>
2016-05-28 16:14:49 +0200

    Add base tb

---
+++ tb.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.Types.all;
+
+entity tb is 
+end entity;
+
+architecture beh of tb is
+begin
+end architecture beh;
