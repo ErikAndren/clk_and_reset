a65b78a66ba3a5655b65b047e75d3df8d4932866
Erik Zachrisson <erik@zachrisson.info>
2016-05-28 17:02:52 +0200

    Fix eth clk driving

--- tb.vhd
+++ tb.vhd
@@ -9,23 +9,23 @@ entity tb is
 end entity;
 
 architecture beh of tb is
-  signal sys_clk_100_MHz : bit1;
-  signal tck_20_MHz      : bit1;
-  signal eth_clk_10_MHz  : bit1;
-  signal tc_clk_5_MHz    : bit1;
-  signal rst             : bit1;
-  signal soft_reset      : bit1;
-
+  signal sys_clk_100_MHz   : bit1;
+  signal tck_20_MHz        : bit1;
+  signal eth_clk_10_MHz    : bit1;
+  signal tc_clk_5_MHz      : bit1;
+  signal rst               : bit1;
+  signal soft_reset        : bit1;
+  --
   signal sdram_clk_100_MHz : bit1;
-  signal dbg_tck_20_MHz : bit1;
-  signal wb_clk_50_MHz : bit1;
-  signal wb_rst_lvl1 : bit1;
-  signal wb_rst_lvl2 : bit1;
-  signal eth_rst : bit1;
-  signal eth_phy_rst : bit1;
-  signal tc_clk_o_5_MHz : bit1;
-  signal sys_rst_sync : bit1;
-  signal eth_clk_pad : bit1;
+  signal dbg_tck_20_MHz    : bit1;
+  signal wb_clk_50_MHz     : bit1;
+  signal wb_rst_lvl1       : bit1;
+  signal wb_rst_lvl2       : bit1;
+  signal eth_rst           : bit1;
+  signal eth_phy_rst       : bit1;
+  signal tc_clk_o_5_MHz    : bit1;
+  signal eth_clk_o_10_MHz  : bit1;
+  signal sys_rst_sync      : bit1;
 
 begin
   sys_clk_gen : process
@@ -68,14 +68,14 @@ begin
     end loop;
   end process;
   
-  soft_reset <= '0', '1' after 200 ns, '0' after 100 ns;
+  soft_reset <= '0', '1' after 200 ns, '0' after 300 ns;
   rst <= '1', '0' after 100 ns;
 
   dut : entity work.clk_and_reset
     port map (
       sys_clk_pad_i  => sys_clk_100_MHz,
       tck_pad_i      => tck_20_MHz,
-      eth_clk_pad_i    => eth_clk_10_MHz,
+      eth_clk_pad_i  => eth_clk_10_MHz,
       rst_pad_i      => rst,
       soft_reset_i   => soft_reset,
       tc_clk_i       => tc_clk_5_MHz,
@@ -86,7 +86,7 @@ begin
       wb_rst_lvl1_o  => wb_rst_lvl1,
       wb_rst_lvl2_o  => wb_rst_lvl2,
       --
-      eth_clk_o      => eth_clk_10_MHz,
+      eth_clk_o      => eth_clk_o_10_MHz,
       eth_rst_o      => eth_rst,
       eth0_phy_rst_n => eth_phy_rst,
       --
